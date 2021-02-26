005be85c9b2e71a326a3ba161d3e2c0616826c87
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2021-02-25 16:42:02 -0800

    edit page updated

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,10 +10,24 @@ class RestaurantsController < ApplicationController
   def new
   end
 
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
   def create
     @restaurant = Restaurant.new(restaurant_params)
     @restaurant.save
     redirect_to @restaurant
+  end
+  
+  def update
+    @restaurant = Restaurant.find(params[:id])
+   
+    if @restaurant.update(restaurant_params)
+      redirect_to @restaurant
+    else
+      render 'edit'
+    end
   end
 
   private
